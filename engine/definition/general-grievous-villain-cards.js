243c010668727b3e06bb01865f705f28907b41ae
Milosz <miloszd@kth.se>
2025-01-07 21:18:26 -0600

    Add intial General Grievous definition

---
+++ engine/definition/general-grievous-villain-cards.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "B1 Battle Droids": {
+        "name": "B1 Battle Droids",
+        "card-type": "Ally",
+        "credit-cost": 1,
+        "strength": 0,
+        "description": "B1 Battle Droids get +1 Strength for each other Ally at their location.",
+    },
+
+    "BX-Droid Commandos": {
+        "name": "BX-Droid Commandos",
+        "card-type": "Ally",
+        "credit-cost": 2,
+        "strength": 2,
+        "description":
+            "When BX-Droid Commandos would be discarded in a Vanquish action, you may put them into your hand instead.",
+    },
+};
